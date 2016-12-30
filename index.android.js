e67ee7f0e7679d950befe7470dd564b854ef98e9
Cary <guojiashuang@live.com>
2016-12-30 16:56:37 +0800

    修改

--- index.android.js
+++ index.android.js
@@ -10,31 +10,57 @@ import {
   StyleSheet,
   Text,
   View,
-  Image
+  Image,
+  ListView
 } from 'react-native';
-var DATA={
+var DATA=[
+	{
     "id":"1",
     "name":"欢迎来到我的世界",
     "text":"黎明快要到来，世界将被拯救，冲锋吧，勇士！",
     "img":"http://image.tianjimedia.com/uploadImages/2015/182/51/E3I1U9WNCC1M_games-wallpapers-minecraft-wallpaper-wallpaper-36344.jpg",
     "add_time":"2016-09-03",
     "url":"https://github.com/CaryGo"
-};
+},
+{
+    "id":"1",
+    "name":"欢迎来到我的世界",
+    "text":"黎明快要到来，世界将被拯救，冲锋吧，勇士！",
+    "img":"http://image.tianjimedia.com/uploadImages/2015/182/51/E3I1U9WNCC1M_games-wallpapers-minecraft-wallpaper-wallpaper-36344.jpg",
+    "add_time":"2016-09-03",
+    "url":"https://github.com/CaryGo"
+}
+];
+
 class exercise extends Component {
+	getInintinalState(){
+		var ds = new ListView.DataSource({rowHasChanged:(r1,r2) => r1!==r2});
+		return{
+			dataSource:ds.cloneWithRows(DATA),
+		};
+	}
   render() {
     return (
       <View style={styles.container}>
-            <Image source={{uri:DATA.img}} style={styles.images}/>
-        <Text style={styles.name}>
-            {DATA.name}
-        </Text>
-        <Text style={styles.text}>
-            {DATA.text}
-        </Text>
-        <Text style={styles.time}>
-            {DATA.add_time}
-        </Text>
-      </View>
+        <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.item}/>
+      	</View>
+    );
+  }
+  item() {
+    return (
+      <View style={styles.container}>
+        	<Text style={styles.name}>
+            haha
+        	</Text>
+        	<Text style={styles.text}>
+            heihei
+        	</Text>
+        	<Text style={styles.time}>
+            2016-09-05
+        	</Text>
+      	</View>
     );
   }
 }
@@ -42,8 +68,6 @@ class exercise extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-//    justifyContent: 'center',
-//    alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
   name: {
@@ -64,10 +88,10 @@ const styles = StyleSheet.create({
     color:'#286754',
      marginLeft: 10,
   },
-     images: {
-                                 height:200,
-                                 margin:10,
-     },
+  images:{
+  	height:200,
+  	margin:10,
+  },
 });
 
 AppRegistry.registerComponent('exercise', () => exercise);
